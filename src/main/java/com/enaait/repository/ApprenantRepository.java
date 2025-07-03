499e8255f9d35870704b24102a2e2e5279a706d1
mohamed <iaazi.mohamed.1998@gmail.com>
2025-07-03 17:47:52 +0100

    create apprenets repository

---
+++ src/main/java/com/enaait/repository/ApprenantRepository.java
@@ -0,0 +1,7 @@
+package com.enaait.repository;
+
+import com.enaait.model.Apprenant;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ApprenantRepository extends JpaRepository<Apprenant, Long> {
+} 
