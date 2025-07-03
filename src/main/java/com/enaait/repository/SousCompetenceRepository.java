38e5bc1c1358300b60472bc39a388304e71c3b91
mohamed <iaazi.mohamed.1998@gmail.com>
2025-07-03 17:49:33 +0100

    create souscompetance model class est repository

---
+++ src/main/java/com/enaait/repository/SousCompetenceRepository.java
@@ -0,0 +1,7 @@
+package com.enaait.repository;
+
+import com.enaait.model.SousCompetence;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SousCompetenceRepository extends JpaRepository<SousCompetence, Long> {
+} 
