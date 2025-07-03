c27d819e2a4038b5fb7d59eec058095acf1d87e1
mohamed <iaazi.mohamed.1998@gmail.com>
2025-07-03 17:48:52 +0100

    create competance  repository

---
+++ src/main/java/com/enaait/repository/CompetenceRepository.java
@@ -0,0 +1,7 @@
+package com.enaait.repository;
+
+import com.enaait.model.Competence;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CompetenceRepository extends JpaRepository<Competence, Long> {
+} 
