b0a227955e6beddb4ed9d444f44d4ef9fc098af5
mohamed <iaazi.mohamed.1998@gmail.com>
2025-07-10 10:10:03 +0100

    create the competence repository

--- CompetenceMicroService/src/main/java/com/medo/competencemicroservice/repository/CompetenceRepository.java
+++ CompetenceMicroService/src/main/java/com/medo/competencemicroservice/repository/CompetenceRepository.java
@@ -1,4 +1,10 @@
 package com.medo.competencemicroservice.repository;
 
-public class CompetenceRepository {
+import com.medo.competencemicroservice.model.Competence;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CompetenceRepository extends JpaRepository<Competence,String> {
+
 }
