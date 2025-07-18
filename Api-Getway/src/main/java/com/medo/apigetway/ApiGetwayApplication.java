8880930c2af20dec2cb522ced53f659720181b6c
mohamed <iaazi.mohamed.1998@gmail.com>
2025-07-18 17:16:32 +0100

    add getway dependencie

--- Api-Getway/src/main/java/com/medo/apigetway/ApiGetwayApplication.java
+++ Api-Getway/src/main/java/com/medo/apigetway/ApiGetwayApplication.java
@@ -2,12 +2,12 @@ package com.medo.apigetway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ApiGetwayApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ApiGetwayApplication.class, args);
     }
-
 }
