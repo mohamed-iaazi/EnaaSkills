7ac3fbece3959c71bf997f5bdc6bafa5fab57e79
mohamed <iaazi.mohamed.1998@gmail.com>
2025-07-18 16:54:56 +0100

    create the eureke server microservice

--- Enaaskils/src/main/java/com/enaait/repository/ApprenantRepository.java
+++
@@ -1,9 +0,0 @@
-package com.enaait.repository;
-
-import com.enaait.model.Apprenant;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ApprenantRepository extends JpaRepository<Apprenant, Long> {
-} 
