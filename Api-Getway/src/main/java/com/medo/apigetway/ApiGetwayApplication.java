d456d151f2986499d74ee80a31f296bc538c8254
mohamed <iaazi.mohamed.1998@gmail.com>
2025-07-23 11:34:43 +0100

    f

--- Api-Getway/src/main/java/com/medo/apigetway/ApiGetwayApplication.java
+++ Api-Getway/src/main/java/com/medo/apigetway/ApiGetwayApplication.java
@@ -2,10 +2,10 @@ package com.medo.apigetway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableEurekaServer
+@EnableDiscoveryClient
 public class ApiGetwayApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiGetwayApplication.class, args);
--- CompetenceMicroService/src/main/java/com/medo/competencemicroservice/CompetenceMicroServiceApplication.java
+++ CompetenceMicroService/src/main/java/com/medo/competencemicroservice/CompetenceMicroServiceApplication.java
@@ -2,9 +2,10 @@ package com.medo.competencemicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CompetenceMicroServiceApplication {
 
     public static void main(String[] args) {
